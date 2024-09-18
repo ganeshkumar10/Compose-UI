b63f8d60822bcdfb60070020d04954f67f87a563
ganeshkumar10 <spartanganesh96@gmail.com>
2024-09-18 12:58:21 +0530

    Initial commit added with text and images and listview in compose UI

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,15 @@
+
+m
+=app/src/main/java/com/example/blogapplication/MainActivity.kt,a\4\a489a6cbd58fba9746517a666b3362155c63f13a
+o
+?app/src/main/java/com/example/blogapplication/ui/theme/Color.kt,1\9\1997c08027893694d154c8b1f7f39583f613eb53
+o
+?app/src/main/java/com/example/blogapplication/ui/theme/Theme.kt,c\5\c5a7b2a762188e00c70cd022a4da848ae6d5f0c7
+n
+>app/src/main/java/com/example/blogapplication/ui/theme/Type.kt,6\3\637c2c52d5a8445b581b1cde23dff03f53144ae3
+D
+app/build.gradle.kts,d\2\d2a4383452aad84a18429a5844d31330a012a9e6
+@
+build.gradle.kts,d\b\dbcff70658daf80b53ce624f6adcaa529df5ed8d
+F
+app/proguard-rules.pro,9\e\9e08934d811afe28fbc77aaa3c0d747b94348db9
---
+++ app/src/main/java/com/example/blogapplication/MainActivity.kt
@@ -0,0 +1,118 @@
+package com.example.blogapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.blogapplication.ui.theme.BlogApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BlogApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Greeting("Hello Android")
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        LazyRow(modifier = Modifier.fillMaxWidth()) {
+            items(name.length) { _ ->
+                Icon(
+                    Icons.Default.Add, contentDescription = null,
+                    modifier = Modifier.size(100.dp)
+                )
+            }
+        }
+        Column(modifier = Modifier.fillMaxHeight()) {
+            Text(
+                text = "Hello $name!",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.Red)
+                    .padding(10.dp),
+                color = Color.Black,
+                fontFamily = FontFamily.Monospace,
+                fontSize = 16.sp,
+                lineHeight = 16.sp,
+                maxLines = 5,
+                textAlign = TextAlign.Start
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = "Hello!",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.Blue)
+                    .padding(10.dp),
+                color = Color.Black,
+                fontFamily = FontFamily.Monospace,
+                fontSize = 16.sp,
+                lineHeight = 16.sp,
+                maxLines = 5,
+                textAlign = TextAlign.End
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = null,
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            if (name.length > 5) {
+                Icon(
+                    Icons.Default.ArrowBack, contentDescription = null,
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .size(100.dp)
+                )
+            } else {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = name,
+                )
+            }
+        }
+    }
+}
+
+@Preview(
+    showBackground = true
+)
+@Composable
+fun GreetingPreview() {
+    BlogApplicationTheme {
+        Greeting("Testing")
+    }
+}
