f6588a6664fc25981efe4c655880ec428346a209
ganeshkumar10 <spartanganesh96@gmail.com>
2024-09-19 19:33:36 +0530

    added simple button click with increase numbers

--- app/src/main/java/com/example/blogapplication/MainActivity.kt
+++ app/src/main/java/com/example/blogapplication/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -13,14 +14,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -38,8 +43,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             BlogApplicationTheme {
                 // A surface container using the 'background' color from the theme
-                Greeting("Hello Android")
-
+//                Greeting("Hello Android")
+                SampleApp()
             }
         }
     }
@@ -113,6 +118,27 @@ fun Greeting(name: String) {
 @Composable
 fun GreetingPreview() {
     BlogApplicationTheme {
-        Greeting("Testing")
+//        Greeting("Testing")
+        SampleApp()
+    }
+}
+
+@Composable
+fun SampleApp() {
+    var count by remember {
+        mutableStateOf(0)
+    }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = count.toString(), fontSize = 16.sp)
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(onClick = {
+            count++
+        }) {
+            Text(text = "Click me!:  $count".uppercase())
+        }
     }
 }
