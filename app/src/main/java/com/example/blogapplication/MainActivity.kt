0b1105ac25bf35b0b051bcc65f652f9a738ecb69
ganeshkumar10 <spartanganesh96@gmail.com>
2024-09-20 19:38:15 +0530

    Added sample UI with default button and list and edittext

--- app/src/main/java/com/example/blogapplication/MainActivity.kt
+++ app/src/main/java/com/example/blogapplication/MainActivity.kt
@@ -1,40 +1,31 @@
 package com.example.blogapplication
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
+import androidx.compose.material3.Divider
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.blogapplication.ui.theme.BlogApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,75 +33,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BlogApplicationTheme {
-                // A surface container using the 'background' color from the theme
-//                Greeting("Hello Android")
-                SampleApp()
+                SampleDesignWithListAndButtonToAdd()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Column(modifier = Modifier.fillMaxSize()) {
-        LazyRow(modifier = Modifier.fillMaxWidth()) {
-            items(name.length) { _ ->
-                Icon(
-                    Icons.Default.Add, contentDescription = null,
-                    modifier = Modifier.size(100.dp)
-                )
-            }
-        }
-        Column(modifier = Modifier.fillMaxHeight()) {
-            Text(
-                text = "Hello $name!",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(Color.Red)
-                    .padding(10.dp),
-                color = Color.Black,
-                fontFamily = FontFamily.Monospace,
-                fontSize = 16.sp,
-                lineHeight = 16.sp,
-                maxLines = 5,
-                textAlign = TextAlign.Start
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(
-                text = "Hello!",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(Color.Blue)
-                    .padding(10.dp),
-                color = Color.Black,
-                fontFamily = FontFamily.Monospace,
-                fontSize = 16.sp,
-                lineHeight = 16.sp,
-                maxLines = 5,
-                textAlign = TextAlign.End
-            )
-            Spacer(modifier = Modifier.height(20.dp))
-            Image(
-                painter = painterResource(id = R.drawable.ic_launcher_background),
-                contentDescription = null,
-            )
-            Spacer(modifier = Modifier.height(20.dp))
-            if (name.length > 5) {
-                Icon(
-                    Icons.Default.ArrowBack, contentDescription = null,
-                    modifier = Modifier
-                        .align(Alignment.CenterHorizontally)
-                        .size(100.dp)
-                )
-            } else {
-                Image(
-                    painter = painterResource(id = R.drawable.ic_launcher_background),
-                    contentDescription = name,
-                )
-            }
-        }
-    }
-}
 
 @Preview(
     showBackground = true
@@ -118,27 +46,60 @@ fun Greeting(name: String) {
 @Composable
 fun GreetingPreview() {
     BlogApplicationTheme {
-//        Greeting("Testing")
-        SampleApp()
+        SampleDesignWithListAndButtonToAdd()
     }
 }
 
 @Composable
-fun SampleApp() {
-    var count by remember {
-        mutableStateOf(0)
+fun SampleDesignWithListAndButtonToAdd() {
+    val context = LocalContext.current
+    var name by remember {
+        mutableStateOf("")
+    }
+    var names by remember {
+        mutableStateOf(listOf<String>())
     }
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
     ) {
-        Text(text = count.toString(), fontSize = 16.sp)
-        Spacer(modifier = Modifier.height(20.dp))
-        Button(onClick = {
-            count++
-        }) {
-            Text(text = "Click me!:  $count".uppercase())
+        Row {
+            OutlinedTextField(
+                value = name,
+                onValueChange = { text ->
+                    name = text
+                }, modifier = Modifier.weight(1f)
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+            Button(onClick = {
+                if (name.isNotBlank()) {
+                    names = names + name
+                    name = ""
+                } else {
+                    Toast.makeText(context, "Empty text", Toast.LENGTH_SHORT).show()
+                }
+            }) {
+                Text(text = "Add here")
+            }
+        }
+        NameList(names = names)
+    }
+}
+
+@Composable
+fun NameList(
+    names: List<String>,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(modifier) {
+        items(names) { currentName ->
+            Text(
+                text = currentName, modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            )
+            Divider()
         }
     }
 }
