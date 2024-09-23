9560eef3930b8219851c5dcaf6eff689aee17b07
ganeshkumar10 <spartanganesh96@gmail.com>
2024-09-23 18:59:31 +0530

    Added sample UI with text fields and to change a colors for the text

--- app/src/main/java/com/example/blogapplication/MainActivity.kt
+++ app/src/main/java/com/example/blogapplication/MainActivity.kt
@@ -1,39 +1,42 @@
 package com.example.blogapplication
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
-import androidx.compose.material3.Divider
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.blogapplication.ui.theme.BlogApplicationTheme
+import com.example.blogapplication.ui.theme.newBlack
+import com.example.blogapplication.ui.theme.newBlue
+import com.example.blogapplication.ui.theme.newOrange
+import com.example.blogapplication.ui.theme.newWhite
+import com.example.blogapplication.ui.theme.newYellow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BlogApplicationTheme {
-                SampleDesignWithListAndButtonToAdd()
             }
         }
     }
@@ -46,60 +49,60 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingPreview() {
     BlogApplicationTheme {
-        SampleDesignWithListAndButtonToAdd()
+        CustomizedDesignForTextFields()
     }
 }
 
 @Composable
-fun SampleDesignWithListAndButtonToAdd() {
-    val context = LocalContext.current
-    var name by remember {
+fun CustomizedDesignForTextFields() {
+    var firstTextName by remember {
         mutableStateOf("")
     }
-    var names by remember {
-        mutableStateOf(listOf<String>())
-    }
-    Column(
+
+    Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .background(color = Color.White)
     ) {
-        Row {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+                .background(Color.Red)
+        ) {
+            Spacer(modifier = Modifier.height(100.dp))
             OutlinedTextField(
-                value = name,
-                onValueChange = { text ->
-                    name = text
-                }, modifier = Modifier.weight(1f)
+                value = firstTextName,
+                onValueChange = { newValue ->
+                    firstTextName = newValue
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(5.dp),
+                textStyle = TextStyle(fontSize = 16.sp),
+                /*
+                                label = { Text(text = "Enter Name") },
+                */
+                placeholder = {
+                    Text(
+                        text = "Enter text here",
+                        textAlign = TextAlign.Start,
+                        modifier = Modifier.padding(start = 50.dp)
+                    )
+                },
+                colors = TextFieldDefaults.colors(
+                    /*focusedPlaceholderColor = newYellow,
+                    unfocusedPlaceholderColor = newWhite,*/
+                    /*unfocusedContainerColor = newWhite,
+                    focusedContainerColor = newOrange,*/
+                    /*focusedIndicatorColor = newYellow,
+                    unfocusedIndicatorColor = newBlue*/
+                    /*focusedTextColor = Color.Red,
+                    unfocusedTextColor = Color.White*/
+                ),
+                shape = RoundedCornerShape(16.dp)
             )
-            Spacer(modifier = Modifier.width(10.dp))
-            Button(onClick = {
-                if (name.isNotBlank()) {
-                    names = names + name
-                    name = ""
-                } else {
-                    Toast.makeText(context, "Empty text", Toast.LENGTH_SHORT).show()
-                }
-            }) {
-                Text(text = "Add here")
-            }
         }
-        NameList(names = names)
     }
 }
 
-@Composable
-fun NameList(
-    names: List<String>,
-    modifier: Modifier = Modifier
-) {
-    LazyColumn(modifier) {
-        items(names) { currentName ->
-            Text(
-                text = currentName, modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp)
-            )
-            Divider()
-        }
-    }
-}
